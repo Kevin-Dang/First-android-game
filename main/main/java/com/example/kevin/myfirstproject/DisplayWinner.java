a8ae32d061809d1af1fef145d6615bf47c9e9b7e
Kevin-Dang <kevindang1993@gmail.com>
2016-07-11 15:54:37 -0400

    Add files via upload

---
+++ main/main/java/com/example/kevin/myfirstproject/DisplayWinner.java
@@ -0,0 +1,22 @@
+package com.example.kevin.myfirstproject;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class DisplayWinner extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_display_winner);
+    }
+
+    public void restart(View view)
+    {
+        Intent restartIntent = new Intent(this,TitleScreen.class);
+        startActivity(restartIntent);
+
+    }
+}
